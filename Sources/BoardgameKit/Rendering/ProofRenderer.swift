e859404923067cc33dafd3d8c7be4b6dff46aded
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2021-05-07 15:37:15 +0200

    Add work on custom font support

--- Sources/BoardgameKit/Rendering/ProofRenderer.swift
+++ Sources/BoardgameKit/Rendering/ProofRenderer.swift
@@ -35,6 +35,7 @@ final class ProofRenderer: Renderer {
             fatalError()
         }
         try FileManager.default.copyItem(at: templateSiteUrl, to: siteUrl)
+        var discoveredFonts: [URL] = []
         // provided that an URL for .bundle/Contents/Resources has been specified,
         // copy over all files as-is to the proof directory
         if let resourceUrl = resourceUrl {
@@ -49,10 +50,33 @@ final class ProofRenderer: Renderer {
                 #endif
                 try FileManager.default.copyItem(
                     at: resource,
-                    to: siteUrl.appendingPathComponent(resource.lastPathComponent)
+                    to: siteUrl.appendingPathComponent(
+                        resource.lastPathComponent
+                    )
                 )
             }
+
+            if let enumerator = FileManager.default.enumerator(at: resourceUrl, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                for case let fileURL as URL in enumerator {
+                    do {
+                        let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                        if fileAttributes.isRegularFile! {
+                            if fileURL.pathExtension == "ttf" {
+                                discoveredFonts.append(
+                                    URL(string:
+                                    fileURL.path.replacingOccurrences(
+                                        of: resourceUrl.path + "/",
+                                        with: "")
+                                        )!
+                                )
+                            }
+                        }
+                    } catch { print(error, fileURL) }
+                }
+            }
+
         }
+
         let indexUrl = siteUrl.appendingPathComponent("index.html")
         let index = try String(contentsOf: indexUrl, encoding: .utf8)
 
@@ -63,10 +87,32 @@ final class ProofRenderer: Renderer {
         )
         try doc.html.write(to: indexUrl, atomically: true, encoding: .utf8)
 
+        let fontFaces: String
+        if discoveredFonts.isEmpty {
+            fontFaces = "/* no font-faces */"
+        } else {
+            var adasd: String = ""
+            for font in discoveredFonts {
+                // todo: note that this solution does not support mixing styles/weights for the same font
+                //       this is only a problem if you want to mix styles inline, otherwise you can simply set the font separately
+                //       i.e. MyFont and MyFont-Italic
+                //       not sure how to solve without exposing an interface to set @font-faces manually
+                adasd += """
+                @font-face {
+                  font-family: \(font.lastPathComponent.replacingOccurrences(of: "." + font.pathExtension, with: ""));
+                  src: url(\(font.path));
+                }
+                """
+                // todo: if not last, add newlines as appropriate
+            }
+            fontFaces = adasd
+        }
+
         let cssUrl = siteUrl.appendingPathComponent("index.css")
         let css = try String(contentsOf: cssUrl, encoding: .utf8)
             .replacingOccurrences(of: "{{pw}}", with: paper.extent.width.css)
             .replacingOccurrences(of: "{{ph}}", with: paper.extent.height.css)
+            .replacingOccurrences(of: "{{fonts}}", with: fontFaces) // todo: should also apply to image rendering
         try css.write(to: cssUrl, atomically: true, encoding: .utf8)
 
         finishRendering()
